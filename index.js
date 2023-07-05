b946b195532f29e8de7856d9a0cc313f16bc7aad
Jeremy Bernier <jeremy.s.bernier@gmail.com>
2023-07-05 17:06:04 +0900

    show existing projects and use it to determine whether to create new one

--- index.js
+++ index.js
@@ -16,29 +16,40 @@ function query(question) {
 }
 
 async function init() {
-  const projectName = await query("Project name: ");
+  const projectsListOut = await exec(`gcloud projects list`);
+  const projectIds = projectsListOut.stdout
+    .split("\n")
+    .map((line) => line.split(" ")[0])
+    .slice(1)
+    .filter((val) => val !== "");
 
-  const projectExists = await query("Does this project already exist? (y/N) ");
+  const projectIdsMap = new Set(projectIds);
 
-  if (!projectExists || projectExists.toLowerCase() === "n") {
+  console.log("Google Cloud Project IDs", projectIds);
+
+  const projectId = await query("Project ID: ");
+
+  if (!projectIdsMap.has(projectId)) {
+    await query(
+      "Project does not exist so new project will be created (press enter to continue)"
+    );
     console.log("Creating project...");
-    await exec(`gcloud projects create ${projectName}`);
-
+    await exec(`gcloud projects create ${projectId}`);
     console.log("Project created!");
   }
 
-  await exec(`gcloud config set project ${projectName}`);
+  await exec(`gcloud config set project ${projectId}`);
 
   console.log("Enabling Cloud Resource Manager API");
 
   await exec(`gcloud services enable cloudresourcemanager.googleapis.com`);
 
   open(
-    `https://console.cloud.google.com/billing/linkedaccount?project=${projectName}`
+    `https://console.cloud.google.com/billing/linkedaccount?project=${projectId}`
   );
 
   await query(
-    `Please enable billing on your account by going to: https://console.cloud.google.com/billing/linkedaccount?project=${projectName}
+    `Please enable billing on your account by going to: https://console.cloud.google.com/billing/linkedaccount?project=${projectId}
     
     When you're finished, press the enter key to continue...`
   );
@@ -49,7 +60,9 @@ async function init() {
     `gcloud services enable cloudbuild.googleapis.com run.googleapis.com containerregistry.googleapis.com`
   );
 
-  const serviceAccountName = await query(`Service account name: `);
+  const serviceAccountName = await query(
+    `Service account name (must be between 6-30 characters): `
+  );
   const serviceDescription = await query(`Service description: `);
   const serviceDisplayName = await query(`Service display name: `);
 
@@ -59,22 +72,22 @@ async function init() {
   --description="${serviceDescription}" \
   --display-name="${serviceDisplayName}"`);
 
-  const serviceAccountEmail = `${serviceAccountName}@${projectName}.iam.gserviceaccount.com`;
+  const serviceAccountEmail = `${serviceAccountName}@${projectId}.iam.gserviceaccount.com`;
   console.log(`Service account email: ${serviceAccountEmail}`);
 
   console.log(
     "Granting the service account Give the service account Cloud Run Admin, Storage Admin, and Service Account User roles."
   );
 
-  await exec(`gcloud projects add-iam-policy-binding ${projectName} \
+  await exec(`gcloud projects add-iam-policy-binding ${projectId} \
   --member=serviceAccount:${serviceAccountEmail} \
   --role=roles/run.admin
 
-gcloud projects add-iam-policy-binding ${projectName} \
+gcloud projects add-iam-policy-binding ${projectId} \
   --member=serviceAccount:${serviceAccountEmail} \
   --role=roles/storage.admin
 
-gcloud projects add-iam-policy-binding ${projectName} \
+gcloud projects add-iam-policy-binding ${projectId} \
   --member=serviceAccount:${serviceAccountEmail} \
   --role=roles/iam.serviceAccountUser`);
 
@@ -90,7 +103,7 @@ gcloud projects add-iam-policy-binding ${projectName} \
   console.log(
     "When setting up CI/CD (eg. with GitHub actions), use the following variables:"
   );
-  console.log(`GCP_PROJECT_ID=${projectName}`);
+  console.log(`GCP_PROJECT_ID=${projectId}`);
   console.log(`GCP_APP_NAME=`);
   console.log(`GCP_EMAIL=${serviceAccountEmail}`);
   console.log(`GCP_CREDENTIALS=<Insert key.json contents>`);
